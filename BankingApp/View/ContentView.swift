a36940d363d72d8a6471832ad9bb10fecaff1e4f
Jannatpapia <papiamcode@gmail.com>
2022-03-19 21:31:36 +0600

    Added contentView

--- BankingApp/View/ContentView.swift
+++ BankingApp/View/ContentView.swift
@@ -9,8 +9,18 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ScrollView(.vertical) {
+            ScrollView(.horizontal, showsIndicators: false) {
+                LazyHStack {
+                    ForEach(0..<10, id: \.self) { item in
+                        CardView(isSelected: item == 0 )
+                            .shadow(color: Color.black.opacity(0.2), radius: 5, x: 5, y: 5)
+                    }
+                }
+                .padding(20)
+            }
+        }
+        .background(Color.white)
     }
 }
 
