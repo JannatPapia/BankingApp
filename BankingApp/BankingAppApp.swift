498df2bab39c3de4780f91e85746dc1e79b7ec53
Jannatpapia <papiamcode@gmail.com>
2022-03-19 17:38:05 +0600

    Project SetUp

---
+++ BankingApp/BankingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BankingAppApp.swift
+//  BankingApp
+//
+//  Created by Jannatun Nahar Papia  on 19/3/22.
+//
+
+import SwiftUI
+
+@main
+struct BankingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
