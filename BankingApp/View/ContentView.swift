f23e7f5f428e49269135eb8b8a69cc0ff417e865
Jannatpapia <papiamcode@gmail.com>
2022-03-19 23:36:45 +0600

    8:42 Updating: cardlist

--- BankingApp/View/ContentView.swift
+++ BankingApp/View/ContentView.swift
@@ -12,30 +12,47 @@ struct ContentView: View {
         NavigationView{
             ZStack {
                 Color.white.edgesIgnoringSafeArea(.all)
-        ScrollView(.vertical) {
-//            HStack{
-//                Text("My Card")
-//                    .font(.title)
-//                    .fontWeight(.bold)
-//                Spacer()
-//            }
-//            .padding()
-            ScrollView(.horizontal, showsIndicators: false) {
-                LazyHStack {
-                    ForEach(0..<10, id: \.self) { item in
-                        CardView(isSelected: item == 0 )
-                            .shadow(color: Color.black.opacity(0.2), radius: 5, x: 5, y: 5)
-                    }
+                ScrollView(.vertical) {
+                    CardList()
+                    TransactionsView()
+                    //            HStack{
+                    //                Text("My Card")
+                    //                    .font(.title)
+                    //                    .fontWeight(.bold)
+                    //                Spacer()
+                    //            }
+                    //            .padding()
+//                    ScrollView(.horizontal, showsIndicators: false) {
+//                        LazyHStack {
+//                            ForEach(0..<10, id: \.self) { item in
+//                                CardView(isSelected: item == 0 )
+//                                    .shadow(color: Color.black.opacity(0.2), radius: 5, x: 5, y: 5)
+//                            }
+//                        }
+//                        .padding(20)
+//                    }
                 }
-                .padding(20)
+                //      .background(Color.white)
+                .navigationTitle(Text("My Card"))
+     //           .background(Color.yellow)
             }
-        }
-  //      .background(Color.white)
-        .navigationTitle("My Card")
-    }
         }
     }
 }
+//
+//struct CardList: View {
+//    var body: some View {
+//        ScrollView(.horizontal, showsIndicators: false) {
+//            LazyHStack {
+//                ForEach(0..<10, id: \.self) { item in
+//                    CardView(isSelected: item == 0 )
+//                        .shadow(color: Color.black.opacity(0.2), radius: 5, x: 5, y: 5)
+//                }
+//            }
+//            .padding(20)
+//        }
+//    }
+//}
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
--- BankingApp/View/TransactionView.swift
+++ BankingApp/View/TransactionView.swift
@@ -12,7 +12,7 @@ struct TransactionsView: View {
     var body: some View {
         LazyVStack {
             ForEach(0..<10, id: \.self) { item in
-                
+                TransactionRow()
             }
         }
     }
@@ -22,12 +22,28 @@ struct TransactionRow: View {
     var body: some View {
         HStack {
             Image("visa")
-            VStack {
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 30, height: 30)
+                .padding(5)
+            VStack(alignment: .leading) {
                 Text("Dribble")
+                    .font(.headline)
                 Text("12 Nov 2020 11: 08")
+                    .font(.caption)
+                    .foregroundColor(Color(.secondaryLabel))
             }
             Spacer()
             Text("-$60.0")
+                .font(.headline)
         }
+        .padding(8)
+    }
+}
+
+struct TransactionsView_Previews: PreviewProvider {
+    static var previews: some View {
+        TransactionsView()
+            .previewLayout(.sizeThatFits)
     }
 }
